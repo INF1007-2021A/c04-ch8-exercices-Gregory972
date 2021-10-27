677a3be8d5974ac16b862c2fee203184577c9361
Gregory972 <91223311+Gregory972@users.noreply.github.com>
2021-10-27 10:54:50 -0400

    test

--- exercice.py
+++ exercice.py
@@ -7,6 +7,15 @@ PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80
 
 
 # TODO: Définissez vos fonction ici
+def compare(fichier1, fichier2):
+    f1 = open(fichier1, "r", encoding="utf-8")
+    f2 = open(fichier2, "r", encoding="utf-8")
+    for lettre1, lettre2 in f1, f2:
+        if lettre1 != lettre2:
+
+            return 'Les fichiers sont différents à partir de :', (lettre1, lettre2)
+    return 'Les fichiers sont identiques'
+
 
 
 if __name__ == '__main__':
